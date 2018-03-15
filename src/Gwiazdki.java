c34b4f8a9d7bd9d579bb431ac5f144ab5fa2d209
Arcon <arek@websitebest.pl>
2018-03-15 21:44:58 +0100

    cwiczenia 04

---
+++ src/Gwiazdki.java
@@ -0,0 +1,18 @@
+public class Gwiazdki{
+
+
+    public static void main(String[] args) {
+        int wymiar=4;
+        for (int i =0;i<wymiar;i++)
+        {
+                        for (int j =0; j<wymiar;j++)
+                           {
+                          if (j<=i)
+                            System.out.print("*");
+                             }
+                System.out.println();
+        }
+
+
+         }
+}
