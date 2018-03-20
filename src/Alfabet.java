6ef21b1dd36bf6c65d52535b0c4a9d6d49dd6210
Arcon <arek@websitebest.pl>
2018-03-20 18:52:52 +0100

    cwiczenia 06

---
+++ src/Alfabet.java
@@ -0,0 +1,13 @@
+public class Alfabet
+{
+    public static void main(String[] args) {
+        char alfabet[] = new char[1];
+        for (int i=0;i<alfabet.length;i++) {
+            for (char x = 'a'; x <= 'z'; x++)
+
+            {
+                System.out.print(alfabet[i]=x );
+            }
+        }
+    }
+}
